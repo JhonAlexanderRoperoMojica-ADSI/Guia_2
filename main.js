ef7176d54ee554d99c0689c63bfadce5f7a1a0e4
Anju0307 <jropero111@gmail.com>
2022-10-09 18:23:17 -0500

    punto 5

--- main.js
+++ main.js
@@ -1,34 +1,19 @@
-function comprobar(seleccionado) {
-    if (seleccionado == "Triangulo") {
-        document.querySelector("#formTriangulo").style.display = 'block';
-    }
-    document.querySelector("#formCirculo").style.display = 'block';
-}
-let seleccionado;
 addEventListener("DOMContentLoaded", (e) => {
-    let dato = document.querySelector("#dato");
-    dato.addEventListener("change", (e) => {
-        document.querySelector("#formTriangulo").style.display = 'none';
-        document.querySelector("#formCirculo").style.display = 'none';
-        seleccionado = dato.value;
-        comprobar(seleccionado);
-    })
-})
-addEventListener("DOMContentLoaded", (e) => {
-    let area = document.querySelector("#area");
-    area.addEventListener("submit", (e) => {
+    let formulario = document.querySelector("#form");
+    formulario.addEventListener("submit", (e) => {
         e.preventDefault();
-        if (seleccionado == "Triangulo") {
-            let base = document.querySelector("#Base").value;
-            let altura = document.querySelector("#Altura").value;
-            let area = (base * altura) / 2;
-            document.querySelector("#resultado").innerHTML = area;
-        } else if (seleccionado == "Circulo") {
-            let radio = document.querySelector("#Radio").value;
-            let area = Math.PI * radio * radio;
-            document.querySelector("#resultado").innerHTML = area;
-        } else {
-            alert("No se puede acceder si escoger una figura...")
+        validar();
+        function validar(){
+            let lado1 = document.getElementById("numero1").value;
+            let lado2 = document.getElementById("numero2").value;
+
+            if((lado1 > 0) && (lado2 > 0)){
+                let area = lado1 * lado2;
+                document.getElementById('resultado').innerHTML = `El área del rectángulo es: ${area}`;
+            }
+            else{
+                document.getElementById('resultado').innerHTML = `Los lados deben ser positivos.`;
+            }
         }
     })
 })
